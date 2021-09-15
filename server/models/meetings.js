0dc502b2dc1d72559b9cb0bc15f2bbbfcbe791c5
jeens <jeens93@gmail.com>
2021-09-15 15:25:02 +0200

    #2 Refactor meeting schema

--- server/models/meetings.js
+++ server/models/meetings.js
@@ -3,10 +3,10 @@ const { Schema } = mongoose;
 
 const meetingSchema = new Schema({
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    firstPossibleDay: { type: String },
-    lastPossibleDay: { type: String },
-    firstPossibleHour: { type: String },
-    lastPossibleHour: { type: String },
+    firstPossibleDay: { type: Date, required: true, min: '2021-09-15' },
+    lastPossibleDay: { type: Date, required: true, min: '2021-09-15' },
+    firstPossibleHour: { type: Number, required: true, min: 0, max: 23 },
+    lastPossibleHour: { type: Number, required: true, min: 0, max: 23 },
     meetingName: { type: String },
     participantsList: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
 });
