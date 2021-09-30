b3334c2c4d29fa14d97ec80b2c47f11a717e9a1a
Jens Sjödin <jeens93@gmail.com>
2021-09-30 14:46:16 +0200

    #13: Style the footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,8 +2,15 @@
   <div id="app">
     <!-- Render the content of the current page view -->
     <router-view />
+    <Footer />
   </div>
 </template>
+<script>
+import Footer from './components/Footer.vue'
+export default {
+  components: { Footer }
+}
+</script>
 <style>
 #app {
   -webkit-font-smoothing: antialiased;
--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
-    <footer class="footer">
-      <p>Hellooooooo</p>
+    <footer v-if="isLoggedIn()" class="footer">
+      <p>All rights reserved 2021</p>
+      <p>Contact us at: MaidenUpName@email.com</p>
     </footer>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    isLoggedIn() {
+      return this.$store.getters.isLoggedIn
+    }
+  }
+}
 </script>
+
+<style scoped>
+.footer {
+  display: flex;
+  align-self: flex-end;
+  justify-content: space-between;
+  color: #2c3e50;
+  padding: 10%;
+}
+</style>
