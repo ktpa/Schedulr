49f5ceb065c4837d57aa51a9f81a2f9fb74ebc22
numank <numank@student.chalmers.se>
2021-10-01 01:30:21 +0200

    #20: Use cookies instead of localStorage
    Local storage doesn't provide any funcionalities
    to remove tokens when they expire. However on cookies,
    we can define expiration date.

--- client/src/main.js
+++ client/src/main.js
@@ -4,17 +4,21 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import { Api } from '@/Api.js'
 import store from './api/store'
+import VueCookies from 'vue-cookies'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
+Vue.use(VueCookies)
+
+Vue.$cookies.config('30d')
 
 Vue.config.productionTip = false
 
 Vue.prototype.$http = Api
 
-const token = localStorage.getItem('token')
+const token = Vue.$cookies.get('token')
 if (token) {
   Vue.prototype.$http.defaults.headers.common.Authorization = `Bearer ${token}`
 }
