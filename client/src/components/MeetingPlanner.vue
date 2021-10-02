6c139c6097075cdeb0a2d9d9cf5c581e4c56bb5d
nikarl <nikarl@student.chalmers.se>
2021-10-02 07:36:12 +0200

    Added the MeetingPlanner with basic front-end functionality through dates.

---
+++ client/src/components/MeetingPlanner.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div>
+      <h2>Meeting date range</h2>
+      <DatePicker v-model="range" :model-config="modelConfig" is-range />
+    </div>
+    <div>
+      <b-button>Hours</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { DatePicker } from 'v-calendar'
+
+export default {
+  name: 'MeetingPlanner',
+  components: {
+    DatePicker
+  },
+  data() {
+    return {
+      range: {
+        start: Date.now(),
+        end: Date.now() + 3
+      },
+      modelConfig: {
+        start: {
+          timeAdjust: '00:00:00'
+        },
+        end: {
+          timeAdjust: '23:59:59'
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style></style>
