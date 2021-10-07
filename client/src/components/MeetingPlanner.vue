27d4a491027a039c707493a2034e251050b2d071
nikarl <nikarl@student.chalmers.se>
2021-10-07 14:09:55 +0200

    Current meeting progress

--- client/src/components/MeetingPlanner.vue
+++ client/src/components/MeetingPlanner.vue
@@ -1,31 +1,36 @@
 <template>
   <div>
-    <div v-if="!this.hoursToggle">
-      <h2>Meeting date range</h2>
+    <div>
+      <h2>Select a meeting date</h2>
       <DatePicker
-        data-date-picker="deactivated"
-        v-model="range"
-        :select-attribute="selectDragAttribute"
-        :drag-attribute="selectDragAttribute"
-        is-range
+        v-model="date"
         @drag="dragValue = $event"
         :first-day-of-week="2"
         show-iso-weeknumbers
-      >
-        <template v-slot:day-popover="{ format }">
-          <div>
-            {{ format(dragValue ? dragValue.start : range.start, 'MMM D') }}
-            -
-            {{ format(dragValue ? dragValue.end : range.end, 'MMM D') }}
-          </div>
-        </template>
-      </DatePicker>
-    </div>
-    <div v-else>
-      <h2>Hours</h2>
+        class="neatShadow"
+      />
     </div>
     <div>
-      <b-button v-on:click="changeActiveScreen">Next</b-button>
+      <h2>
+        {{ new Intl.DateTimeFormat('en-GB').format(this.date) }}
+      </h2>
+    </div>
+    <div>
+      <b-input
+        class="form-input"
+        type="text"
+        v-model="form.name"
+        placeholder="Meeting name"
+        id="name"
+      />
+    </div>
+    <div v-if="this.incorrect">
+      <p class="errorText">Date has not been set.</p>
+    </div>
+    <div>
+      <b-button v-on:click="createMeeting()" class="confirmBtn"
+        >Create Meeting</b-button
+      >
     </div>
   </div>
 </template>
@@ -35,16 +40,17 @@ import { DatePicker } from 'v-calendar'
 
 export default {
   name: 'MeetingPlanner',
+  incorrect: false,
   components: {
     DatePicker
   },
   data() {
     return {
-      hoursToggle: false,
-      range: {
-        start: Date.now(),
-        end: new Date(Date.now() + 5)
+      form: {
+        name: ''
       },
+      meeting: '',
+      date: Date.now(),
       modelConfig: {
         start: {
           timeAdjust: '00:00:00'
@@ -56,27 +62,42 @@ export default {
     }
   },
   methods: {
-    changeActiveScreen() {
-      if (!this.hoursToggle) {
-        this.hoursToggle = true
-        this.registerDates()
-      } else this.hoursToggle = false
-    },
-    registerDates() {
-      console.log(this.range.start)
-    }
-  },
-  computed: {
-    selectDragAttribute() {
-      return {
-        popover: {
-          visibility: 'hover',
-          isInteractive: false
+    createMeeting() {
+      if (this.date instanceof Date) {
+        console.log('Date is defined.')
+        this.incorrect = false
+        this.meeting = {
+          userID: this.$store.userID,
+          day: this.date,
+          firstPossibleHour: 0,
+          lastPossibleHour: 23,
+          meetingName: this.form.meetingName,
+          participantsList: this.$store.userID
         }
+      } else {
+        console.log('Date is not defined.')
+        this.incorrect = true
       }
     }
   }
 }
 </script>
 
-<style></style>
+<style scoped>
+.form-input {
+  max-width: 25em;
+  -webkit-box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
+  -moz-box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
+  box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
+}
+.confirmBtn {
+  -webkit-box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
+  -moz-box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
+  box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
+}
+.neatShadow {
+  -webkit-box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
+  -moz-box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
+  box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
+}
+</style>
--- client/src/views/CreateMeeting.vue
+++ client/src/views/CreateMeeting.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>
+  <h1 class="landing">
     <MeetingPlanner />
   </h1>
 </template>
