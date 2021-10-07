efa790f982377c9ba99a81b44c67a7115d072a13
nikarl <nikarl@student.chalmers.se>
2021-10-07 14:23:13 +0200

    Final fix for rebase.

--- client/src/components/MeetingPlanner.vue
+++ client/src/components/MeetingPlanner.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-<<<<<<< HEAD
     <div>
       <h2>Select a meeting date</h2>
       <DatePicker
@@ -32,34 +31,6 @@
       <b-button v-on:click="createMeeting()" class="confirmBtn"
         >Create Meeting</b-button
       >
-=======
-    <div v-if="!this.hoursToggle">
-      <h2>Meeting date range</h2>
-      <DatePicker
-        data-date-picker="deactivated"
-        v-model="range"
-        :select-attribute="selectDragAttribute"
-        :drag-attribute="selectDragAttribute"
-        is-range
-        @drag="dragValue = $event"
-        :first-day-of-week="2"
-        show-iso-weeknumbers
-      >
-        <template v-slot:day-popover="{ format }">
-          <div>
-            {{ format(dragValue ? dragValue.start : range.start, 'MMM D') }}
-            -
-            {{ format(dragValue ? dragValue.end : range.end, 'MMM D') }}
-          </div>
-        </template>
-      </DatePicker>
-    </div>
-    <div v-else>
-      <h2>Hours</h2>
-    </div>
-    <div>
-      <b-button v-on:click="changeActiveScreen">Next</b-button>
->>>>>>> b765c0a34f44325a5ac929c1725859323a24b76c
     </div>
   </div>
 </template>
@@ -69,28 +40,17 @@ import { DatePicker } from 'v-calendar'
 
 export default {
   name: 'MeetingPlanner',
-<<<<<<< HEAD
   incorrect: false,
-=======
->>>>>>> b765c0a34f44325a5ac929c1725859323a24b76c
   components: {
     DatePicker
   },
   data() {
     return {
-<<<<<<< HEAD
       form: {
         name: ''
       },
       meeting: '',
       date: Date.now(),
-=======
-      hoursToggle: false,
-      range: {
-        start: Date.now(),
-        end: new Date(Date.now() + 5)
-      },
->>>>>>> b765c0a34f44325a5ac929c1725859323a24b76c
       modelConfig: {
         start: {
           timeAdjust: '00:00:00'
@@ -102,7 +62,6 @@ export default {
     }
   },
   methods: {
-<<<<<<< HEAD
     createMeeting() {
       if (this.date instanceof Date) {
         console.log('Date is defined.')
@@ -118,32 +77,12 @@ export default {
       } else {
         console.log('Date is not defined.')
         this.incorrect = true
-=======
-    changeActiveScreen() {
-      if (!this.hoursToggle) {
-        this.hoursToggle = true
-        this.registerDates()
-      } else this.hoursToggle = false
-    },
-    registerDates() {
-      console.log(this.range.start)
-    }
-  },
-  computed: {
-    selectDragAttribute() {
-      return {
-        popover: {
-          visibility: 'hover',
-          isInteractive: false
-        }
->>>>>>> b765c0a34f44325a5ac929c1725859323a24b76c
       }
     }
   }
 }
 </script>
 
-<<<<<<< HEAD
 <style scoped>
 .form-input {
   max-width: 25em;
@@ -162,6 +101,3 @@ export default {
   box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
 }
 </style>
-=======
-<style></style>
->>>>>>> b765c0a34f44325a5ac929c1725859323a24b76c
--- client/src/views/CreateMeeting.vue
+++ client/src/views/CreateMeeting.vue
@@ -1,9 +1,5 @@
 <template>
-<<<<<<< HEAD
   <h1 class="landing">
-=======
-  <h1>
->>>>>>> b765c0a34f44325a5ac929c1725859323a24b76c
     <MeetingPlanner />
   </h1>
 </template>
