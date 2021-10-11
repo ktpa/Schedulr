1dba28f02aecf42d1be8bcfb2e4b8c9f24593116
nikarl <nikarl@student.chalmers.se>
2021-10-11 15:43:25 +0200

    Further alignment with home page, WIP state.

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="home">
     <b-list-group id="meetings_list">
-      <b-list-group-item v-for="meeting in meetings" :key="meeting.message">
-        <a :href="`meeting/${meeting._id}`">
-          <span class="name">{{ meeting.meetingName }}</span>
-          <span class="participants"
-            >Participants: {{ meeting.participantsList.length }}</span
-          >
-        </a>
-      </b-list-group-item>
+      <div class="outerMeetingContainer">
+        <b-list-group-item v-for="meeting in meetings" :key="meeting.message">
+          <a :href="`meeting/${meeting._id}`">
+            <div class="innerNameContainer">
+              <span class="name">{{ meeting.meetingName }}</span>
+            </div>
+            <div class="innerParticipantsContainer">
+              <span class="participants"
+                >Participants: {{ meeting.participantsList.length }}</span
+              >
+            </div>
+          </a>
+        </b-list-group-item>
+      </div>
     </b-list-group>
   </div>
 </template>
@@ -44,42 +50,49 @@ export default {
 
 <style scoped>
 #meetings_list {
-  display: flex;
-  align-self: flex-end;
   align-items: center;
   justify-content: center;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+}
+.outerMeetingContainer {
+  width: 40%;
+}
+.innerParticipantsContainer {
+  text-align: right;
+  background-color: rgba(255, 0, 0, 0.3);
+}
+
+.innerNameContainer {
+  text-align: left;
+  background-color: rgba(0, 255, 0, 0.3);
 }
 .home {
   background-image: linear-gradient(45deg, #93a5cf 0%, #e4efe9 100%);
 }
 .name {
-  margin-right: 2em;
+  margin: 2em;
 }
 .participants {
-  margin-left: 2em;
+  margin: 2em;
 }
 .list-group-item {
-  padding: 15px;
-  margin-bottom: 10px;
-  max-width: 40rem;
-  min-width: 20rem;
+  padding: 1em;
+  margin: 0.5em;
 }
 .list-group-item:nth-child(even) {
-  display: flex;
   background-color: rgba(9, 92, 215, 1);
   -webkit-box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
   -moz-box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
   box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
 }
 .list-group-item:nth-child(odd) {
-  display: flex;
   background-color: rgba(8, 83, 196, 1);
   -webkit-box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
   -moz-box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
   box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
 }
 a {
-  display: flex;
   color: rgba(255, 255, 255, 0.87);
   width: 100%;
   justify-content: space-between;
@@ -89,5 +102,6 @@ a:hover {
   text-decoration: none;
   color: white;
   transition: 0.2s;
+  text-shadow: rgba(0, 0, 0, 0.3) 0 1px 5px;
 }
 </style>
